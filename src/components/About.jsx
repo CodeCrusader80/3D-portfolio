6cd20e4fe7dd03800c2b57bb8649e118819f1369
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-09-18 21:50:59 +0200

    Experience section is done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ import {SectionWrapper} from "../hoc";
 
 const ServiceCard = ({index, title, icon}) => {
     return(
-        <Tilt className={"xs:w-[220px] w-full"}>
+        <Tilt className={"xs:w-[200px] w-full"}>
             <motion.div
                 variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
                 className={"w-full green-pink-gradient p–[1px] rounded-[20px] shadow-card"}
