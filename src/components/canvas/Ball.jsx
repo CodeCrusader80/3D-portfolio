e124a1435723ccb98ae93393f79878c507c5c225
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-09-19 17:36:17 +0200

    Ball Effect in Three done

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
-const Ball = () => {
+import {Suspense} from 'react'
+import {Canvas} from "@react-three/fiber";
+import {Decal, Float, OrbitControls, Preload, useTexture} from "@react-three/drei";
+import CanvasLoader from "../Loader.jsx";
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25}/>
+      <directionalLight position={[0, 0, 0.05]}/>
+      <mesh castShadow receiveShadow scale={2.70}>
+        <icosahedronGeometry args={[1, 1]}/>
+        <meshStandardMaterial
+          color='#FFF8EB'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
+};
+
+const BallCanvas = ({ icon }) => {
+  return (
+      <Canvas
+          frameloop='always'
+          dpr={[1, 2]}
+          gl={{preserveDrawingBuffer: true}}
+      >
+        <Suspense fallback={<CanvasLoader/>}>
+          <OrbitControls
+              enableZoom={false}/>
+          <Ball imgUrl={icon}/>
+        </Suspense>
+
+        <Preload all/>
+      </Canvas>
   )
 }
 
-export default Ball
+export default BallCanvas
