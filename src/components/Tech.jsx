e112d2cedc120fe12466dad043cb87ace4f12d80
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-09-19 17:49:16 +0200

    Update Tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -12,6 +12,7 @@ const Tech = () => {
         <h3 className={`${styles.sectionHeader}`}>My Tech Stack</h3>
         <p className={`${styles.sectionSubText}`}>Always prepare for learn 👨‍💻</p>
       </motion.div>
+
       <div className={"flex flex-row flex-wrap justify-center gap-10"}>
         {technologies.map((technology) => (
             <div className={"w-28 h-28"} key={technology.name}>
