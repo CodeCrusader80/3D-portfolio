914ed786d714d09f999aa8db4962aff240bf91ad
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-09-19 17:44:14 +0200

    Update Tech Section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,25 @@
 import {BallCanvas} from "./canvas";
 import {SectionWrapper} from "../hoc";
 import {technologies} from "../constant/index.js";
+import {styles} from "../styles.js";
+import {motion} from "framer-motion";
+import {textVariant} from "../utils/motion.js";
 
 const Tech = () => {
   return (
-    <div className={"flex flex-row flex-wrap justify-center gap-10"}>
-      {technologies.map((technology) => (
-          <div className={"w-28 h-28"} key={technology.name}>
-            <BallCanvas icon={technology.icon}/>
-          </div>
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()} className={"mb-10"}>
+        <h3 className={`${styles.sectionHeader}`}>My Tech Stack</h3>
+        <p className={`${styles.sectionSubText}`}>Always prepare for learn 👨‍💻</p>
+      </motion.div>
+      <div className={"flex flex-row flex-wrap justify-center gap-10"}>
+        {technologies.map((technology) => (
+            <div className={"w-28 h-28"} key={technology.name}>
+              <BallCanvas icon={technology.icon}/>
+            </div>
+        ))}
+      </div>
+    </>
   )
 }
 
