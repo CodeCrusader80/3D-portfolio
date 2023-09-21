82d03888900889294a4d49a539d42acf4d863fdc
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-09-21 21:26:10 +0200

    Some adjust for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,12 +22,12 @@ const Hero = () => {
       </div>
         <ComputersCanvas className={"pointer-events-auto"}/>
 
-        <div className={"absolute xs:bottom-1 bottom-10 w-full flex justify-center items-center"}>
+        <div className={"absolute w-full flex justify-center items-center"}>
             <a href={"#about"}>
                 <div className={"w-[28px] h-[50px] rounded-3xl border-2 border-secondary flex justify-center items-start p-2"}>
                     <motion.div
                         animate={{
-                            y:[0, 10, 0]
+                            y:[0, 15, 0]
                         }}
                         transition={{
                             duration: 1.5,
